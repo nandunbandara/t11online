8702fcde80e5cf6f4473944e0e74b91cfba437f7
root <ntbandara3@hotmail.com>
2017-07-22 19:30:50 +0530

    login controller

--- v2.0/members/app.js
+++ v2.0/members/app.js
@@ -1,8 +1,14 @@
 /**
  * Created by root on 7/22/17.
  */
-'use strict';
+'use strict'
 
 angular.module('t-app',[
 //    dependencies
+    't.loginController'
 ])
+
+.controller('appController',function(){
+    const app = this;
+    app.test = "hello";
+})
--- v2.0/members/app/login/loginController.js
+++ v2.0/members/app/login/loginController.js
@@ -1,3 +1,10 @@
 /**
  * Created by root on 7/22/17.
  */
+'use strict'
+
+angular.module('t.loginController', [])
+
+.controller('loginCtrl', [function(){
+
+}])
