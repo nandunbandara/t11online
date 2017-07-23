ac3e9fd49e568bf2695b7ebceb5c3b716d3969c0
root <ntbandara3@hotmail.com>
2017-07-23 09:26:01 +0530

    firebase configuration

--- v2.0/members/app.js
+++ v2.0/members/app.js
@@ -6,10 +6,23 @@
 angular.module('t-app',[
 //    dependencies
     't.app.routes',
+    'firebase',
     't.loginController'
 
 ])
 
 .controller('appController',function($scope){
     $scope.test = "hello";
+})
+
+.config(function(){
+    var config = {
+        apiKey: "AIzaSyCtH19nWESOiar2x7XcSgyZevbIOvXxkd0",
+        authDomain: "t11online-22539.firebaseapp.com",
+        databaseURL: "https://t11online-22539.firebaseio.com",
+        projectId: "t11online-22539",
+        storageBucket: "",
+        messagingSenderId: "429180692295"
+    };
+    firebase.initializeApp(config);
 })
